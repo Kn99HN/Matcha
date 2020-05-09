4dc1affb8f5809301b61f241458ef4bba2d6eafd
Kn99HN <khanhnguyen99hn@gmail.com>
2020-05-09 10:32:19 -0400

    update data types

--- tencha/parser.hs
+++ tencha/parser.hs
@@ -4,15 +4,13 @@ module Parser where
     Defining expression data types
 -}
 
-data Expr = Const 
+data Expr = 
+          AConst Int
+        | BConst Bool
+        | Var String
         | BinOp Bop Expr Expr 
         | UnOp Uop Expr
     deriving(Eq)
-
-data Const = AConst Int 
-            | BConst Bool 
-            | Var String
-            deriving (Eq, Show)
 
 data Bop = Plus | Minus | Times
         | Div | Eq | Ne
@@ -24,6 +22,26 @@ data Bop = Plus | Minus | Times
 data Uop = UNot | UMinus
     deriving (Show, Eq)
 
+plus, minus, times, div, eq, ne, lt :: Expr -> Expr -> Expr
+plus = BinOp Plus -- this is valid because Op Plus is type Expr, which has a function, that takse an Expr and return Expr -> Expr for us
+minus = BinOp Minus
+times = BinOp Times
+div = BinOp Div
+eq = BinOp Eq
+ne = BinOp Ne
+lt = BinOp Lt
+gt = BinOp Gt
+ge = BinOp Ge
+and = BinOp And
+or = BinOp Or
+impl = BinOp Impl
+iff = BinOp Iff
+unot = UnOp UNot
+uminus = UnOp UMinus
+
+
+example :: Expr
+example = times (plus (AConst 1) (AConst 2)) (AConst 3)
 -- instance Show Not where 
 --     show Not = 
 -- main = putStrLn (Not)
