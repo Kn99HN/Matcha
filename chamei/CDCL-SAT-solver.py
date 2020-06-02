e5bd0dc52e43c4c30ca4d950a120dadb280a3441
Kn99HN <khanhnguyen99hn@gmail.com>
2020-06-02 17:58:46 -0400

    starting on cdcl sat solver

--- chamei/CDCL-SAT-solver.py
+++ chamei/CDCL-SAT-solver.py
@@ -1,95 +1,119 @@
 
-def select_literals(clauses):
-    for clause in clauses:
-        for literal in clause:
-            return literal[0]
+# def select_literals(clauses):
+#     for clause in clauses:
+#         for literal in clause:
+#             return literal[0]
         
 
-# finding if any clause has all but one values are false 
-# def unit_prop(clauses) 
+# # finding if any clause has all but one values are false 
+# # def unit_prop(clauses) 
 
-'''
-def CDCL(F, G):
-    if unit_prop(F,G) == CONFLICT: return UNSAT
-    dl = 0
-    while ! allVariablesAssigned(F, G):
-        (x,v) = decide(F,G) #decide stage
-        dl += 1
-        G = G U {(x,v)}
-        if unit_prop(F,G) == CONFLICT:
-            B = conflict_analysis(F,G)
-            if B < 0: return UNSAT
-            else backtrack(F,G, B):
-                dl = B
-    return true
-'''
+# '''
+# def CDCL(F, G):
+#     if unit_prop(F,G) == CONFLICT: return UNSAT
+#     dl = 0
+#     while ! allVariablesAssigned(F, G):
+#         (x,v) = decide(F,G) #decide stage
+#         dl += 1
+#         G = G U {(x,v)}
+#         if unit_prop(F,G) == CONFLICT:
+#             B = conflict_analysis(F,G)
+#             if B < 0: return UNSAT
+#             else backtrack(F,G, B):
+#                 dl = B
+#     return true
+# '''
 
-# Change this to be more pythonic
-def all_variables_assigned(formulas, graph):
-    for i in formulas:
-        ls = formulas.get(i)
-        for clause in ls:
-            for lit in clause:
-                if lit[1] == "undefined": 
-                    return True
-    return False
+# # Change this to be more pythonic
+# def all_variables_assigned(formulas, graph):
+#     for i in formulas:
+#         ls = formulas.get(i)
+#         for clause in ls:
+#             for lit in clause:
+#                 if lit[1] == "undefined": 
+#                     return True
+#     return False
 
-def decide(formulas, graph):
-    for clause in formulas:
-        for lit in clause:
-            if lit[1] == "undefined":
-                return lit[0], True
+# def decide(formulas, graph):
+#     for clause in formulas:
+#         for lit in clause:
+#             if lit[1] == "undefined":
+#                 return lit[0], True
 
-def unit_prop(formulas, variables, graph):
-    for variable in variables:
-        ls = variables[variable]
-        for i in ls:
-            clause = formulas.get(i)
-            counter = 0
-            variable = ""
-            for lit in clause:
-                if lit[1] == False:
-                    counter += 1
-                elif lit[1] == True:
-                    return "SAT"
-            if counter == len(clause):
-                return "CONFLICT"
-            elif counter == len(clause) - 1:
-                return "UNIT"
-    return "UNRESOLVED"
+# def unit_prop(formulas, variables, graph):
+#     for variable in variables:
+#         ls = variables[variable]
+#         for i in ls:
+#             clause = formulas.get(i)
+#             counter = 0
+#             variable = ""
+#             for lit in clause:
+#                 if lit[1] == False:
+#                     counter += 1
+#                 elif lit[1] == True:
+#                     return "SAT"
+#             if counter == len(clause):
+#                 return "CONFLICT"
+#             elif counter == len(clause) - 1:
+#                 return "UNIT"
+#     return "UNRESOLVED"
 
-def Node(var, value, dl):
-    return (var, value,dl)
+# def Node(var, value, dl):
+#     return (var, value,dl)
+
+
+class Node: 
+    def __init__(self, name, value, dl, antecedent, negate):
+        self.name = name
+        self.value = value
+        self.dl = dl
+        self.antecedent = antecedent
+        self.negate = negate
+    
+    def __str__(self):
+        return "(" + self.name + "," + self.value + "," + self.dl + "," + self.antecedent + ")"
 
 # Change to None later
 file = open("test.txt", "r")
 with file as f: 
     content = [line.rstrip() for line in f]
     formula = {}
-    variables = {}
+    literals = {}
     form = ''
-    counter = 0
+    counter = 1
     for command in content:
         values = command.split(' ')
         if 'p' in values:
             form = values[1]
         if 'c' not in values and 'p' not in values:
-            clause = set()
             for val in values:
                 if val is not '0':
-                    node = Node(val, "undefined", -1)
-                    clause.add(node)
-            formula[counter] = clause
-            counter += 1
-    for clause in formula:
-        ls = formula.get(clause)
-        for lit in ls:
-            value = lit[0]
-            if '-' in value:
-                value = value.split('-')[1]
-            if variables.get(value):
-                variables.get(value).append(clause)
-            else:
-                variables[value] = [clause]
+                    if '-' in val:
+                        lit = val.split('-')[1]
+                    else:
+                        lit = val
 
-print(unit_prop(formula, variables, ""))
+                    if counter in formula:
+                        formula.get(counter).add(Node(val, None, None, None, None))
+                    else:
+                        formula[counter] = {Node(val, None, None, None, None)}
+                    
+                    if lit in literals:
+                        ls = literals.get(lit)
+                        if counter not in ls:
+                            literals.get(lit).append(counter)
+                    else:
+                        literals[lit] = [counter]
+            counter += 1
+    
+    print(formula)
+    print(literals)
+
+def has_unassigned_var(formula):
+    for clause in formula:
+        for lit in clause:
+            if lit.value == None:
+                return True
+    return False
+
+
--- chamei/IGraph.py
+++ chamei/IGraph.py
@@ -1,16 +1,35 @@
 class IGraph:
     def IGraph(self):
-        nodes = list()
-
-    def add(self,node):
-        literal = {node: []}
-        self.nodes.append(literal)
+        graph_dict = {}
+        self.graph_dict = graph_dict
     
+    def add_vertex(self, node):
+        if node not in self.graph_dict:
+            graph_dict[node] = []
     
+    def add_edges(self, nil_literal, false_literals, edges):
+        bool_val = False
+        '''
+        It would be best if the node in formula has the same data structure as the one in the graph.
+        It would eliminate lots of complexity dealing with dictionary. 
+        '''
+        for literal in false_literals:
+            for vertex in self.graph_dict:
+                if literal[0] == vertex.name and (not literal[1]) == vertex.value:
+                    bool_val = True
+                else:
+                    bool_val = False
+        if bool_val:
+            for literal in false_literals:
+                if nil_literal not in self.graph_dict:
+                    self.graph_dict[nil_literal] = []
+                else:
+                    self.graph_dict.get(literal).append(nil_literal)
     
 
 class Node: 
     def Node(self, value, dl, antecedent):
+        self.name = name
         self.value = value
         self.dl = dl
         self.antecedent = antecedent
